ca856e1de765719a3eee9bd906bcdab22656b4d7
JungHoon99 <greenjm1@nate.com>
2023-10-15 13:29:48 +0900

    User serializer 생성

---
+++ web_site/rufas/serializers.py
@@ -0,0 +1,12 @@
+﻿from rest_framework import serializers
+from rufas.models import User
+
+class UserSeriailzer(serializers.Serializer):
+    class Meta:
+        model = User
+        fields = ['userid', 'pw1', 'pw2', 'name', 'address', 'gender', 'phone']
+
+class UserLoginSeriailzer(serializers.Serializer):
+    class Meta:
+        model = User
+        fields = ['userid', 'pw']
