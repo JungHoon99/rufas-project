cb39bef2658879321ad0d641a70dca52967e6ff4
JungHoon99 <greenjm1@nate.com>
2023-10-22 16:44:28 +0900

    cr구현

--- api/rufas/urls.py
+++ api/rufas/urls.py
@@ -5,5 +5,5 @@ from rufas.views import UserCreateView, UserLoginView, ServiceViewSet
 urlpatterns = [
     path('signup/', UserCreateView.as_view(), name='signup-user'),
     path('login/', UserLoginView.as_view(), name='login-user'),
-    path('service/',ServiceViewSet.as_view(), name='service'),
+    path('service/',ServiceViewSet.as_view({'get':'list','post':'create'}), name='service'),
 ]
