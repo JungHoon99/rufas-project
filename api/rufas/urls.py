ae3eb6a217b888b2a2f958e7a47a4c68d9a5ca94
JungHoon99 <greenjm1@nate.com>
2023-10-22 14:59:36 +0900

    rufas url 연결

---
+++ api/rufas/urls.py
@@ -0,0 +1,8 @@
+﻿from django.urls import path
+from rufas.views import UserCreateView, UserLoginView
+
+
+urlpatterns = [
+    path('signup/', UserCreateView.as_view(), name='signup-user'),
+    path('login/', UserLoginView.as_view(), name='login-user'),
+]
