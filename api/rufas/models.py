2cca5b36c4057699fc4872933816e5ee21891888
JungHoon99 <greenjm1@nate.com>
2023-10-22 14:18:24 +0900

    user serializser 작성

--- api/rufas/models.py
+++ api/rufas/models.py
@@ -9,13 +9,16 @@ import uuid
 # Create your models here.
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, date_of_birth, password=None):
+    def create_user(self, email, username, phone, gender, date_of_birth, password=None):
         if not email:
             raise ValueError('Users must have an email address')
 
         user = self.model(
             email=self.normalize_email(email),
             date_of_birth=date_of_birth,
+            username=username,
+            phone=phone,
+            gender=gender,
         )
 
         user.set_password(password)
@@ -44,6 +47,8 @@ class User(AbstractBaseUser):
         max_length=255,
         unique=True,
     )
+    username = models.CharField(max_length=30, null=False)
+    phone = models.CharField(max_length=13, null=False)
     date_of_birth = models.DateField()
     gender = models.IntegerField(choices=USER_GENDER_LABEL, default=1)
     is_active = models.BooleanField(default=True)
---
+++ api/rufas/serializers.py
@@ -0,0 +1,53 @@
+﻿from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import authenticate
+
+from rufas.models import User
+
+class UserSeriailzer(serializers.ModelSerializer):
+    pw1 = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password] # 비밀번호 유효성 검사
+    )
+    pw2 = serializers.CharField(write_only=True, required=True) # 비밀번호 확인
+
+    class Meta:
+        model = User
+        fields = ('email', 'pw1', 'pw2', 'username', 'gender', 'phone')
+
+    def validate(self, data):
+        if data['pw1'] != data['pw2']:
+            raise serializers.ValidationError({"password": "비밀번호가 일치하지 않습니다."})
+        return data
+
+    def create(self, validated_data):
+        # create 요청에 대해 create 메소드를 오버라이딩, 유S저를 생성하고 토큰을 생성함.
+        user = User.objects.create(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            gender=validated_data['gender'],
+            phone=validated_data['phone'] 
+        )
+        user.set_password(validated_data['pw1'])
+        user.save()
+        return user
+
+class UserLoginSeriailzer(TokenObtainPairSerializer):
+    userid = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+    class Meta:
+        model = User
+        fields = ['userid', 'password']
+
+    def validate(self, attrs):
+        user = authenticate(**attrs)
+
+        if user:
+            data = super().validate(attrs)
+            data['user'] = user
+            return data
+        else:
+            raise serializers.ValidationError("아이디 혹은 비밀번호가 일치하지 않습니다.")
