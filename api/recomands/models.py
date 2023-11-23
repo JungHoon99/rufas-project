db7e5b8961baa6da449a26974fcba11955fe5f03
JungHoon99 <greenjm1@nate.com>
2023-11-23 11:11:46 +0900

    modify

--- api/recomands/models.py
+++ api/recomands/models.py
@@ -4,25 +4,24 @@ from django_neomodel import DjangoNode
 import neomodel
 from neomodel import StructuredNode, StringProperty, RelationshipTo, RelationshipFrom, FloatProperty, Relationship
 
-class ItemNode(StructuredNode):
-    name = StringProperty(unique_index=True)
-    purchased_by = RelationshipTo('UserNode', 'PURCHASED_BY')
-    rated_by = RelationshipFrom('UserNode', 'RATED_BY', cardinality=neomodel.OneOrMore)
-
 
 class RatingNode(StructuredNode):
     uid = StringProperty(unique_index=True)
     value = FloatProperty()
 
     # Relationship with UserNode and ItemNode
-    user = Relationship('UserNode', 'RATED_BY')
-    item = Relationship('ItemNode', 'RATED_ITEM')
+    user = RelationshipTo('UserNode', 'RATED_BY')
+    item = RelationshipTo('ItemNode', 'RATED_ITEM')
 
+class ItemNode(StructuredNode):
+    name = StringProperty(unique_index=True)
+    purchased_by = RelationshipTo('UserNode', 'PURCHASED_BY')
+    rated_by = Relationship('RatingNode', 'RATED_ITEM', cardinality=neomodel.OneOrMore)
 
-class UserNode(DjangoNode):
+class UserNode(StructuredNode):
     username = StringProperty(unique_index=True)
     purchases = RelationshipTo(ItemNode, 'PURCHASED')
-    ratings = RelationshipTo(RatingNode, 'RATED_BY', cardinality=neomodel.OneOrMore)
+    ratings = Relationship(RatingNode, 'RATED_BY', cardinality=neomodel.OneOrMore)
 
 
 class Recommendation(DjangoNode):
--- api/recomands/views.py
+++ api/recomands/views.py
@@ -5,12 +5,21 @@ from rest_framework.response import Response
 from recomands.models import UserNode, ItemNode, Recommendation, RatingNode
 from recomands.serializers import CreateUserItemWithRatingSerializer
 import json
-import numpy as np
+import math
+import requests
 
 from neomodel import db
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+def scale_to_minus_one_one(x):
+    x -= 10
+    # 아크탄젠트 함수를 사용하여 -π/2에서 π/2 사이의 각도를 구함
+    angle = math.atan(x)
+    
+    # 구한 각도를 -1에서 1 사이로 스케일링
+    scaled_value = (2 / math.pi) * angle
+    
+    return scaled_value
+
 
 def calculate_similarity(user1, user2):
     if user1.username == user2.username:
@@ -55,7 +64,7 @@ class RecommendationView(APIView):
         most_similar_user = max(all_users, key=lambda other_user: calculate_similarity(user, other_user))
 
         # Get items rated by the most similar user
-        similar_user_rated_item_names = set([item.name for rating in most_similar_user.ratings.all() for item in rating.item.all() if rating.value >= 2.5])
+        similar_user_rated_item_names = set([item.name for rating in most_similar_user.ratings.all() for item in rating.item.all() if rating.value >= 2.3])
 
         # Filter items not rated by the current user
         unrated_item_names = set([item.name for rating in user.ratings.all() for item in rating.item.all()])
@@ -81,55 +90,101 @@ class CreateUserItemWithRatingView(APIView):
         rating = request.data.get('rating', 0.0)  # Default rating to 0.0 if not provided
 
         try:
-            existing_user = UserNode.nodes.filter(username=user_data['username']).first()
+            existing_user = UserNode.nodes.filter(username=user_data).first()
             if existing_user:
                 new_user = existing_user
             else:
                 raise UserNode.DoesNotExist  # Raise the exception to handle the case of non-existing user
         except UserNode.DoesNotExist:
-            new_user = UserNode(username=user_data['username'])
+            new_user = UserNode(username=user_data)
             new_user.save()
 
         # Check if the item already exists
         try:
-            existing_item = ItemNode.nodes.filter(name=item_data['name']).first()
+            existing_item = ItemNode.nodes.filter(name=item_data).first()
             if existing_item:
                 new_item = existing_item
             else:
                 raise ItemNode.DoesNotExist  # Raise the exception to handle the case of non-existing item
         except ItemNode.DoesNotExist:
-            new_item = ItemNode(name=item_data['name'])
+            new_item = ItemNode(name=item_data)
             new_item.save()
 
         # Connect the user and item
         new_user.purchases.connect(new_item)
 
         try:
-            rating_relationship = RatingNode.nodes.filter(uid=user_data['username']+'_'+str(rating)).first()
-        except:
-            rating_relationship = RatingNode(uid=user_data['username']+'_'+str(rating), value=rating)
-            rating_relationship.save()  # Save the rating node first
+            rating_node = RatingNode.nodes.get(uid = user_data+'_'+item_data)
+            if rating_node:
+                # 이미 해당 사용자가 해당 아이템에 대한 평가가 존재하는 경우
+                current_value = rating_node.value
+                new_value = current_value + rating  # 더하거나 빼고 싶은 값 설정
+                if(new_value>5):
+                    rating_node.value = 5
+                else:    
+                    rating_node.value = new_value
+            else:
+                # 해당 사용자가 해당 아이템에 대한 평가가 없는 경우
+                new_value = rating  # 더하거나 빼고 싶은 값 설정
+                rating_node = RatingNode(uid = user_data+'_'+item_data, value=new_value)
+        except RatingNode.DoesNotExist:
+            # 해당 사용자 또는 아이템이 존재하지 않는 경우
+            new_value = rating  # 더하거나 빼고 싶은 값 설정
+            rating_node = RatingNode(uid = user_data+'_'+item_data, value=new_value)
         
-        new_user.ratings.connect(rating_relationship)
-        # Connect the user and item
-        rating_relationship.user.connect(new_user)
-        rating_relationship.item.connect(new_item)
+        rating_node.save()
+
+        rating_node.user.connect(new_user)
+        rating_node.item.connect(new_item)
+
+        new_user.ratings.connect(rating_node)
 
         return Response(status=status.HTTP_201_CREATED)
     
 class track(APIView):
     def post(self, request, *args, **kwargs):
         s = json.loads(request.body.decode('utf-8'))
+        print(s)
         if("http://127.0.0.1:8080/mall/" in s['링크']):
             s['링크'] = s['링크'].replace("http://127.0.0.1:8080/mall/", "")
         
         if("http://127.0.0.1:8080/mall/" in s['링크 클릭']):
             s['링크 클릭'] = s['링크 클릭'].replace("http://127.0.0.1:8080/mall/", "")
+        if('buy' in s['링크 클릭']):
+            id = s['링크 클릭'].split('/')[1]
+            value = s['체류 시간']/1000
+            user_id = s['user_id']
+            value = scale_to_minus_one_one(value)
+            value = value + 3
+            item = "http://127.0.0.1:8080/mall/detail/"+str(id)+"/"
+            url = "http://127.0.0.1:8000/recomand/create-user-item/"
+            data = {"user":user_id, "item": item, "rating": value}
+            response = requests.post(url, json=data)
+            print(response.status_code)
+            pass
+        if('cart' in s['링크 클릭']):
+            id = s['링크 클릭'].split('/')[1]
+            value = s['체류 시간']/1000
+            user_id = s['user_id']
+            value = scale_to_minus_one_one(value)
+            value = value + 2
+            item = "http://127.0.0.1:8080/mall/detail/"+str(id)+"/"
+            url = "http://127.0.0.1:8000/recomand/create-user-item/"
+            data = {"user":user_id, "item": item, "rating": value}
+            response = requests.post(url, json=data)
+            print(response.status_code)
+            pass
+        if("detail" in s['링크 클릭']):
+            id = s['링크 클릭'].split('/')[1]
+            value = s['체류 시간']/1000
+            user_id = s['user_id']
+            value = scale_to_minus_one_one(value)
+            value = value + 0.5
+            item = "http://127.0.0.1:8080/mall/detail/"+str(id)+"/"
+            url = "http://127.0.0.1:8000/recomand/create-user-item/"
+            data = {"user":user_id, "item": item, "rating": value}
+            response = requests.post(url, json=data)
+            print(response.status_code)
         
-        print(s)
 
         return Response({"recommendations": s})
-    
-    def options(self, request, *args, **kwargs):
-        print(request.body)
-        return super().options(request, *args, **kwargs)
